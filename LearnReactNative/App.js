2ef5cebfb6fbd668bc2722ab346d243cc29ec307
kth4249 <desales4249@gmail.com>
2022-01-26 19:00:14 +0900

    chapter 2. Components

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import Greeting from './components/Greeting';
 
-const App = () => {
-  return (
-    <SafeAreaView>
-      <Greeting />
-    </SafeAreaView>
-  );
-};
+const App = () => (
+  <SafeAreaView>
+    <Greeting />
+  </SafeAreaView>
+);
 
 export default App;
