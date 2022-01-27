b0faaabbbd97ef0af41991014f8352cf7247fbde
kth4249 <desales4249@gmail.com>
2022-01-27 14:56:59 +0900

    component basic

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
-import Greeting from './components/Greeting';
+import React, {useState} from 'react';
+import {Button, SafeAreaView} from 'react-native';
+import Box from './components/Box';
 
-const App = () => (
-  <SafeAreaView>
-    <Greeting />
-  </SafeAreaView>
-);
+const App = () => {
+  const [visible, setVisible] = useState(true);
+  const onPress = () => {
+    setVisible(!visible);
+  };
+  return (
+    <SafeAreaView>
+      <Button title="토글" onPress={onPress} />
+      {visible && <Box rounded size="large" color="blue" />}
+    </SafeAreaView>
+  );
+};
 
 export default App;
